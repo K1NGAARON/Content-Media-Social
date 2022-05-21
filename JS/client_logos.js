67daf4e0a76e9bea1b9a871c00826fe1d43254b8
K1NGAARON <gerardaaron1997@gmail.com>
2022-05-21 17:15:25 +0200

    made logos cleaner

--- JS/client_logos.js
+++ JS/client_logos.js
@@ -1,7 +1,7 @@
 const clientLogosWrapper = document.querySelector(".client-logos-wrapper");
 const clientLogos = [
     {
-        'clientIMG' : '../IMG/logo_saumon_dor.png',
+        'clientIMG' : '../IMG/logo_saumon.jpg',
         'clientAlt' : 'Sauman Dor logo',
         'clientHref' : 'https://www.instagram.com/ausaumondorkoksijde/'
     },
@@ -11,72 +11,72 @@ const clientLogos = [
         'clientHref' : 'https://www.instagram.com/feelgood_care/'
     },
     {
-        'clientIMG' : '../IMG/logo_fit_atelier.png',
+        'clientIMG' : '../IMG/logo_fitatelier.jpg',
         'clientAlt' : 'Fit Atelier Koksijde logo',
         'clientHref' : 'https://www.instagram.com/fitatelier_koksijde/'
     },
     {
-        'clientIMG' : '../IMG/logo_cirque.png',
+        'clientIMG' : '../IMG/logo_cirque.jpg',
         'clientAlt' : 'Café Cirque logo',
         'clientHref' : 'https://www.instagram.com/decirque.rsl/'
     },
     {
-        'clientIMG' : '../IMG/logo_carron_marine.png',
+        'clientIMG' : '../IMG/logo_carron.jpg',
         'clientAlt' : 'Carron marine logo',
         'clientHref' : 'https://www.instagram.com/carron.marine/'
     },
     {
-        'clientIMG' : '../IMG/logo_elektro.png',
+        'clientIMG' : '../IMG/logo_elektro.jpg',
         'clientAlt' : 'Elektro Schoonaert logo',
         'clientHref' : 'https://www.instagram.com/elektroschoonaert/'
     },
     {
-        'clientIMG' : '../IMG/logo_slagerij_nils.png',
+        'clientIMG' : '../IMG/logo_nilis.jpg',
         'clientAlt' : 'Slagerij Nils logo',
         'clientHref' : 'https://www.instagram.com/slagerij_nilis/'
     },
     {
-        'clientIMG' : '../IMG/logo_tiger_balm.jpg',
+        'clientIMG' : '../IMG/logo_tiger.jpg',
         'clientAlt' : 'Tiger Balm logo',
         'clientHref' : 'https://www.instagram.com/tigerbalm_belux/'
     },
     {
-        'clientIMG' : '../IMG/logo_purete.png',
+        'clientIMG' : '../IMG/logo_purete.jpg',
         'clientAlt' : 'Pureté logo',
         'clientHref' : 'https://www.instagram.com/purete_bio/'
     },
     {
-        'clientIMG' : '../IMG/logo_herbal.png',
+        'clientIMG' : '../IMG/logo_herbal.jpg',
         'clientAlt' : 'The Herbalist Gin logo',
         'clientHref' : 'https://www.instagram.com/theherbalistgin/'
     },
     {
-        'clientIMG' : '../IMG/logo_carine.png',
+        'clientIMG' : '../IMG/logo_carine.jpg',
         'clientAlt' : 'Coach Carine logo',
         'clientHref' : 'https://www.instagram.com/coachtherapeutcarine/'
     },
     {
-        'clientIMG' : '../IMG/logo_artsi.png',
+        'clientIMG' : '../IMG/logo_artsi.jpg',
         'clientAlt' : 'Artsi Pasta logo',
         'clientHref' : 'https://www.instagram.com/artsipasta/'
     },
     {
-        'clientIMG' : '../img/logo_omnisoft.png',
+        'clientIMG' : '../IMG/logo_omnisoft.jpg',
         'clientAlt' : 'Omnisoft logo',
         'clientHref' : 'https://www.instagram.com/omnisoftbe/'
     },
     {
-        'clientIMG' : '../IMG/logo_unizo.png',
+        'clientIMG' : '../IMG/logo_unizo.jpg',
         'clientAlt' : 'Unizo Brugge logo',
         'clientHref' : 'https://www.instagram.com/unizobrugge/'
     },
     {
-        'clientIMG' : '../IMG/logo_devlieger.png',
+        'clientIMG' : '../IMG/logo_devlieger.jpg',
         'clientAlt' : 'Devlieger Filip logo',
         'clientHref' : 'https://www.instagram.com/devlieger.knokke/'
     },
     {
-        'clientIMG' : '../IMG/logo_schilderwerken.png',
+        'clientIMG' : '../IMG/logo_schilderwerken.jpg',
         'clientAlt' : 'Schilderwerken Vansteelandt logo',
         'clientHref' : 'https://www.instagram.com/schilderwerkenvansteelandt/'
     },
@@ -86,9 +86,49 @@ const clientLogos = [
         'clientHref' : 'https://www.instagram.com/immoplaza/'
     },
     {
-        'clientIMG' : '../IMG/logo_immo_clevers.png',
+        'clientIMG' : '../IMG/logo_clevers.jpg',
         'clientAlt' : 'Clevers Immobiliën logo',
         'clientHref' : 'https://www.instagram.com/cleversimmobilienbrugge/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_alfons.jpg',
+        'clientAlt' : 'Alfons logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_at.jpg',
+        'clientAlt' : 'AT logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_atrium.jpg',
+        'clientAlt' : 'Atrium logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_cargo.jpg',
+        'clientAlt' : 'Cargo logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_alveringem.jpg',
+        'clientAlt' : 'Alveringem logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_frieke.jpg',
+        'clientAlt' : 'Frieke logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_kathy.jpg',
+        'clientAlt' : 'Kathy logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
+    },
+    {
+        'clientIMG' : '../IMG/logo_verdievel.jpg',
+        'clientAlt' : 'Verdievel logo',
+        'clientHref' : 'https://www.instagram.com/contentmedia_social/'
     }
 ];
 
