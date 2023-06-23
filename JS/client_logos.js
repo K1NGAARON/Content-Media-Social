70b440a0f52f757949d48e4ab7986bf9ca8774af
K1NGAARON <gerardaaron1997@gmail.com>
2023-06-23 14:37:09 +0200

    added new logos

--- JS/client_logos.js
+++ JS/client_logos.js
@@ -30,11 +30,6 @@ const clientLogos = [
         'clientAlt' : 'Elektro Schoonaert logo',
         'clientHref' : 'https://www.instagram.com/elektroschoonaert/'
     },
-    // {
-    //     'clientIMG' : '../IMG/logo_nilis.jpg',
-    //     'clientAlt' : 'Slagerij Nils logo',
-    //     'clientHref' : 'https://www.instagram.com/slagerij_nilis/'
-    // },
     {
         'clientIMG' : '../IMG/logo_tiger.jpg',
         'clientAlt' : 'Tiger Balm logo',
@@ -50,16 +45,6 @@ const clientLogos = [
         'clientAlt' : 'The Herbalist Gin logo',
         'clientHref' : 'https://www.instagram.com/theherbalistgin/'
     },
-    // {
-    //     'clientIMG' : '../IMG/logo_carine.jpg',
-    //     'clientAlt' : 'Coach Carine logo',
-    //     'clientHref' : 'https://www.instagram.com/coachtherapeutcarine/'
-    // },
-    // {
-    //     'clientIMG' : '../IMG/logo_artsi.jpg',
-    //     'clientAlt' : 'Artsi Pasta logo',
-    //     'clientHref' : 'https://www.instagram.com/artsipasta/'
-    // },
     {
         'clientIMG' : '../IMG/logo_omnisoft.jpg',
         'clientAlt' : 'Omnisoft logo',
@@ -85,11 +70,6 @@ const clientLogos = [
         'clientAlt' : 'Immo Plaza logo',
         'clientHref' : 'https://www.instagram.com/immoplaza/'
     },
-    // {
-    //     'clientIMG' : '../IMG/logo_clevers.jpg',
-    //     'clientAlt' : 'Clevers Immobiliën logo',
-    //     'clientHref' : 'https://www.instagram.com/cleversimmobilienbrugge/'
-    // },
     {
         'clientIMG' : '../IMG/logo_alfons.jpg',
         'clientAlt' : 'Alfons logo',
@@ -100,11 +80,6 @@ const clientLogos = [
         'clientAlt' : 'AT logo',
         'clientHref' : 'https://www.instagram.com/contentmedia_social/'
     },
-    // {
-    //     'clientIMG' : '../IMG/logo_atrium.jpg',
-    //     'clientAlt' : 'Atrium logo',
-    //     'clientHref' : 'https://www.instagram.com/contentmedia_social/'
-    // },
     {
         'clientIMG' : '../IMG/logo_cargo.jpg',
         'clientAlt' : 'Cargo logo',
@@ -140,11 +115,6 @@ const clientLogos = [
         'clientAlt' : 'La Glace De Fie Logo',
         'clientHref' : 'https://www.instagram.com/laglacedefie/'
     },
-    // {
-    //     'clientIMG' : '../IMG/het_oor.jpg',
-    //     'clientAlt' : 'Het Oor Logo',
-    //     'clientHref' : 'https://www.facebook.com/hetoor.alienvandaele/'
-    // },
     {
         'clientIMG' : '../IMG/maison_violon.jpg',
         'clientAlt' : 'Maison Violon Logo',
@@ -165,41 +135,111 @@ const clientLogos = [
         'clientAlt' : 'Optiek Segers Logo',
         'clientHref' : 'https://www.instagram.com/optiek.segers/'
     },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
-    // {
-    //     'clientIMG' : '../IMG/clients/',
-    //     'clientAlt' : ' Logo',
-    //     'clientHref' : ''
-    // },
+    {
+        'clientIMG' : '../IMG/clients/belgo.jpg',
+        'clientAlt' : 'Belgo Logo',
+        'clientHref' : 'https://www.instagram.com/belgorestaurant/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/bilitis.jpg',
+        'clientAlt' : 'Bilitis Logo',
+        'clientHref' : 'https://www.instagram.com/bilitislingeriebadmode/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/blitz.jpg',
+        'clientAlt' : 'Blitz Media Logo',
+        'clientHref' : 'https://blitz-media.io'
+    },
+    {
+        'clientIMG' : '../IMG/clients/club-benelux.jpg',
+        'clientAlt' : 'Club Benelux Logo',
+        'clientHref' : 'https://www.instagram.com/clubbenelux/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/cornr.jpg',
+        'clientAlt' : 'Cornr Logo',
+        'clientHref' : 'https://www.instagram.com/thecornrhotel/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/david-debal.jpg',
+        'clientAlt' : 'David Debal Logo',
+        'clientHref' : 'https://www.instagram.com/david.debal/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/espero.jpg',
+        'clientAlt' : 'Espero Logo',
+        'clientHref' : 'https://www.instagram.com/espero_oostduinkerke/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/evy-flavour.jpg',
+        'clientAlt' : "Evy's Flavour Logo",
+        'clientHref' : 'https://www.instagram.com/evys_flavour/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/fonkelster.jpg',
+        'clientAlt' : 'Fonkelster Logo',
+        'clientHref' : 'https://www.instagram.com/fonkelster/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/fritesse.jpg',
+        'clientAlt' : 'Fritesse Logo',
+        'clientHref' : 'https://www.instagram.com/fritesse_laroche/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/hauspie.jpg',
+        'clientAlt' : 'By Hauspie Logo',
+        'clientHref' : 'https://www.instagram.com/chateau_briand_by_hauspie/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/helmetplus.jpg',
+        'clientAlt' : 'Helmet Plus Logo',
+        'clientHref' : 'https://www.instagram.com/helmet_plus/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/hotel-cajou.jpg',
+        'clientAlt' : 'Hotel Cajou Logo',
+        'clientHref' : 'https://www.instagram.com/cajou_depanne/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/hotel-europe.jpg',
+        'clientAlt' : 'Hotel Europe Logo',
+        'clientHref' : 'https://www.instagram.com/hoteleurope/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/kimberly.jpg',
+        'clientAlt' : 'Kimberly Logo',
+        'clientHref' : 'https://www.instagram.com/juwelen_kimberly/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/klimatherm.jpg',
+        'clientAlt' : 'Klimatherm Logo',
+        'clientHref' : 'https://www.instagram.com/klimatherm_sanitair/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/nobla.jpg',
+        'clientAlt' : 'Nobla Logo',
+        'clientHref' : 'https://www.instagram.com/nobla_restaurant/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/pladujour.jpg',
+        'clientAlt' : 'Pla du Joer Logo',
+        'clientHref' : 'https://www.instagram.com/pladujoer/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/raditel.jpg',
+        'clientAlt' : 'Raditel Logo',
+        'clientHref' : 'https://www.instagram.com/raditel_adinkerke/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/sancv.jpg',
+        'clientAlt' : 'San cv Logo',
+        'clientHref' : 'https://www.instagram.com/sancv_maldegem/'
+    },
+    {
+        'clientIMG' : '../IMG/clients/spoor.jpg',
+        'clientAlt' : 'Spoor Logo',
+        'clientHref' : 'https://www.instagram.com/maisdoolhofspoor74/'
+    },
     // {
     //     'clientIMG' : '../IMG/clients/',
     //     'clientAlt' : ' Logo',
@@ -212,8 +252,14 @@ const clientLogos = [
     // },
 ];
 
+function shuffleArray(array) {
+    array.sort(() => Math.random() - 0.5);
+};
+
 // Create client logos section
 function createClientLogos() {
+    shuffleArray(clientLogos);
+
     for (let i = 0; i < clientLogos.length; i++) {
         const clientLogoTemplate = `
         <a href="${clientLogos[i].clientHref}" target="_blank">
